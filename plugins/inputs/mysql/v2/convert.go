a6198d60807b59d971ed39b10f24aab5709156ef
Patrick Prasse <pprasse@actindo.de>
2020-03-02 09:39:48 +0100

    Mysql input: add slave status. Fix influxdata#6671

--- plugins/inputs/mysql/v2/convert.go
+++ plugins/inputs/mysql/v2/convert.go
@@ -55,15 +55,19 @@ func ParseValue(value sql.RawBytes) (interface{}, error) {
 		return 0, nil
 	}
 
+	if bytes.EqualFold(value, []byte("Connecting")) {
+		return 2, nil
+	}
+
+	if bytes.EqualFold(value, []byte("Preparing"))  {
+		return 3, nil
+	}
+
 	if val, err := strconv.ParseInt(string(value), 10, 64); err == nil {
 		return val, nil
 	}
 	if val, err := strconv.ParseFloat(string(value), 64); err == nil {
 		return val, nil
-	}
-
-	if len(string(value)) > 0 {
-		return string(value), nil
 	}
 
 	return nil, fmt.Errorf("unconvertible value: %q", string(value))
